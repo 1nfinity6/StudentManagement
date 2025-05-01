ad2d9c556b569e835d20198114c8b50e9a2c6063
yuuuki <vwaa1608@yahoo.co.jp>
2025-05-01 14:04:41 +0900

    【授業5/1】

--- src/main/java/raisetech/student/managerment/Application.java
+++ src/main/java/raisetech/student/managerment/Application.java
@@ -1,15 +1,8 @@
 package raisetech.student.managerment;
 
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import raisetech.student.managerment.data.Student;
-import raisetech.student.managerment.data.StudentsCourses;
-import raisetech.student.managerment.repository.StudentRepository;
 
 @SpringBootApplication
 
--- src/main/java/raisetech/student/managerment/controller/StudentController.java
+++ src/main/java/raisetech/student/managerment/controller/StudentController.java
@@ -1,23 +1,14 @@
 package raisetech.student.managerment.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import raisetech.student.managerment.controller.converter.StudentConverter;
-import raisetech.student.managerment.data.Student;
-import raisetech.student.managerment.data.StudentsCourses;
 import raisetech.student.managerment.domain.StudentDetail;
 import raisetech.student.managerment.service.StudentService;
 
@@ -35,9 +26,9 @@ public class StudentController {
   }
 
   /**
-   * 受講生一覧検索です。 全件検索を行うので、条件指定は行わないものとします。
+   * 受講生詳細の一覧検索です。 全件検索を行うので、条件指定は行わないものとします。
    *
-   * @return 受講生一覧（全件）
+   * @return 受講生詳細一覧（全件）
    */
   @GetMapping("/studentList")
   public List<StudentDetail> getStudentList() {
@@ -45,7 +36,7 @@ public class StudentController {
   }
 
   /**
-   * 受講生検索です。 IDに紐づく任意の受講生の情報を取得します。
+   * 受講生詳細の検索です。 IDに紐づく任意の受講生の情報を取得します。
    *
    * @param id 　検索対象の受講生のID
    * @return 対象の受講生情報（単一）
@@ -56,10 +47,10 @@ public class StudentController {
   }
 
   /**
-   * 受講生を新規登録します。
+   * 受講生詳細のを登録します。
    *
-   * @param studentDetail 登録する受講生とコース情報
-   * @return 登録後の受講生詳細情報
+   * @param studentDetail 受講生詳細
+   * @return 実行結果
    */
   @PostMapping("/registerStudent")
   public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail) {
@@ -68,12 +59,12 @@ public class StudentController {
   }
 
   /**
-   * 既存の受講生情報を更新します。
+   * 受講生詳細の更新を行います。キャンセルフラグの更新も行います。(論理削除)
    *
-   * @param studentDetail 更新対象の受講生とコース情報
-   * @return 更新成功のメッセージ
+   * @param studentDetail 受講生詳細
+   * @return 実行結果
    */
-  @PostMapping("/updateStudent")
+  @PutMapping("/updateStudent")
   public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
     service.updateStudent(studentDetail);
     return ResponseEntity.ok("更新処理が成功しました。");
