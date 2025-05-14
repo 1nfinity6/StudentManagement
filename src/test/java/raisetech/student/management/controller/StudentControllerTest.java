4f877b373bdfa484cfc606f1c1e64120d32728f0
yuuuki <vwaa1608@yahoo.co.jp>
2025-05-14 09:40:36 +0900

    【第28回演習課題】修正

--- src/test/java/raisetech/student/management/controller/StudentControllerTest.java
+++ src/test/java/raisetech/student/management/controller/StudentControllerTest.java
@@ -2,10 +2,8 @@ package raisetech.student.management.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -19,11 +17,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentCourse;
-import raisetech.student.management.domain.StudentDetail;
 import raisetech.student.management.repository.StudentRepository;
 import raisetech.student.management.service.StudentService;
 
@@ -42,103 +39,91 @@ class StudentControllerTest {
 
   @Test
   void 受講生詳細の一覧検索が実行できて空のリストが返ってくること() throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.get("/studentList"))
+    mockMvc.perform(get("/studentList"))
         .andExpect(status().isOk());
 
     verify(service, times(1)).searchStudentList();
   }
 
   @Test
-  void 受講生詳細の取得が実行できて空のStudentDetailが返ってくること() throws Exception {
-    Student student = new Student();
-    student.setId("1");
-    student.setName("江並公史");
-    student.setKanaName("エナミコウジ");
-    student.setNickname("こーじ");
-    student.setRegion("奈良県奈良市");
-    student.setGender("その他");
-    student.setEmail("kouji@exaple.com");
-    student.setAge(25);
-    student.setDeleted(false);
-
-    StudentDetail detail = new StudentDetail();
-    detail.setStudent(student);
-
-    when(service.searchStudent("1")).thenReturn(detail);
-
-    mockMvc.perform(get("/student/1"))
+  void 受講生詳細の検索が実行できて空で返ってくること() throws Exception {
+    String id = "999";
+    mockMvc.perform(get("/student/{id}", id))
         .andExpect(status().isOk());
 
-    verify(service).searchStudent("1");
+    verify(service, times(1)).searchStudent(id);
+  }
+
+
+  @Test
+  void 受講生詳細の登録が実行できて空で返ってくること() throws Exception {
+    mockMvc.perform(post("/registerStudent").contentType(MediaType.APPLICATION_JSON).content(
+            """
+                {
+                    "student": {
+                      "id": "1",
+                      "name" : "江並公史",
+                      "kanaName": "エナミコウジ",
+                      "nickname": "こーじ",
+                      "region": "奈良県奈良市",
+                      "gender": "その他",
+                      "email": "kouji@example.com",
+                      "age": 25,
+                      "remark" : "",
+                      "deleted": false
+                    },
+                    "studentCourseList" : [
+                      {
+                         "courseName" : "Javaスタンダード",
+                         "endDate":  "2024-09-01T00:00:00",
+                         "startDate":  "2024-04-01T00:00:00",
+                         "studentId": "1"
+                      }
+                    ]
+                 }
+                """
+        ))
+        .andExpect(status().isOk());
+    verify(service, times(1)).registerStudent(any());
+  }
+
+
+  @Test
+  void 受講生詳細の更新が実行できて空で返ってくること() throws Exception {
+    mockMvc.perform(put("/updateStudent").contentType(MediaType.APPLICATION_JSON).content(
+            """
+                 {
+                     "student": {
+                       "id": "1",
+                       "name" : "江並公史",
+                       "kanaName": "エナミコウジ",
+                       "nickname": "こーじ",
+                       "region": "奈良県奈良市",
+                       "gender": "その他",
+                       "email": "kouji@example.com",
+                       "age": 25,
+                       "remark" : "",
+                       "deleted": false
+                     },
+                     "studentCourseList" : [
+                       {
+                          "id" : "15",
+                          "courseName" : "Javaスタンダード",
+                          "endDate":  "2024-09-01T00:00:00",
+                          "startDate":  "2024-04-01T00:00:00",
+                          "studentId": "1"
+                       }
+                     ]
+                  }
+                """
+        ))
+        .andExpect(status().isOk());
+
+    verify(service, times(1)).updateStudent(any());
   }
 
   @Test
-  void 受講生詳細の登録が実行できて200で返ってくること() throws Exception {
-    Student student = new Student();
-    student.setId("1");
-    student.setName("江並公史");
-    student.setKanaName("エナミコウジ");
-    student.setNickname("こーじ");
-    student.setRegion("奈良県奈良市");
-    student.setGender("その他");
-    student.setEmail("kouji@example.com");
-    student.setAge(25);
-    student.setDeleted(false);
-
-    StudentDetail input = new StudentDetail();
-    input.setStudent(student);
-
-    when(service.registerStudent(any(StudentDetail.class))).thenReturn(input);
-
-    String json = """
-        {
-          "student": {
-            "id": "1",
-            "name": "江並公史",
-            "kanaName": "エナミコウジ",
-            "nickname": "こーじ",
-            "region": "奈良県奈良市",
-            "gender": "その他",
-            "email": "kouji@example.com",
-            "age": 25,
-            "deleted": false
-          },
-          "studentCourseList": []
-        }
-        """;
-
-    mockMvc.perform(post("/registerStudent")
-            .contentType("application/json")
-            .content(json))
-        .andExpect(status().isOk());
-
-    verify(service).registerStudent(any(StudentDetail.class));
-  }
-
-  @Test
-  void 受講生を更新できること() throws Exception {
-    doNothing().when(service).updateStudent(any(StudentDetail.class));
-
-    String json = """
-        {
-         "student": {
-            "id": "1",
-            "name": "更新後の名前"
-          },
-          "studentCourseList": []
-        }
-        """;
-
-    mockMvc.perform(put("/updateStudent")
-            .contentType("application/json")
-            .content(json))
-        .andExpect(status().isOk());
-
-    verify(service).updateStudent(any(StudentDetail.class));
-  }
-
-  @Test
-  void 受講生詳細の受講生で適切な値を入力チェックに異常が発生しない事() {
+  void 受講生詳細の受講生で適切な値を入力した時にチェックに異常が発生しない事() {
     Student student = new Student();
     student.setId("1");
     student.setName("江並公史");
@@ -168,6 +153,7 @@ class StudentControllerTest {
     student.setDeleted(false);
 
     Set<ConstraintViolation<Student>> violations = validator.validate(student);
+
     assertThat(violations.size()).isEqualTo(1);
     assertThat(violations).extracting("message")
         .containsOnly("数字のみ入力するようにしてください。");
